fc9dfcfccf4efc51d3e61d15b9aece307d479e7f
marcon <marconsolomonyadete@gmail.com>
2025-04-28 12:16:34 -0700

    Added a make payment method and created file writer to write on transaction.csv file by including try catch exception.

--- src/AccountingApplication.java
+++ src/AccountingApplication.java
@@ -25,7 +25,7 @@ public class AccountingApplication {
         if (userInput.equalsIgnoreCase("d")) {
             addDeposit();
         } else if (userInput.equalsIgnoreCase("p")) {
-            System.out.println("Make Payment");
+            makePayment();
         } else if (userInput.equalsIgnoreCase("l")) {
             System.out.println("View Ledge");
         } else if (userInput.equalsIgnoreCase("x")) {
@@ -75,5 +75,41 @@ public class AccountingApplication {
             }
         }
     }
+   public static void makePayment(){
+       Scanner s = new Scanner(System.in);
+       System.out.print("Enter the date (yyyy-mm-dd): ");
+       String date = s.nextLine();
+
+       System.out.print("Enter the time (HH:mm:ss): ");
+       String time = s.nextLine();
+
+       System.out.print("Enter the description: ");
+       String description = s.nextLine();
+
+       System.out.print("Enter the vendor: ");
+       String vendor = s.nextLine();
+
+       System.out.print("Enter the amount: ");
+       double amount = 0.0;
+       if(s.hasNextDouble()){
+           amount = s.nextDouble();
+           s.nextLine();
+
+           amount *= -1;//to make it negative(debt)
+
+       //transaction line
+           String transaction = date+ "|" + time+ "|" + description+ "|" + vendor+ "|" + String.format("$%.2f",amount);
+
+       try {
+           FileWriter fileWriter = new FileWriter("transactions.csv",true);
+           fileWriter.write(transaction + "\n");
+           fileWriter.close();
+           System.out.println("Payment added successfully thanks!");
+
+           }catch (IOException e) {
+           System.out.println("Error writing to file: " + e.getMessage());
+           }
+       }
+   }
 }
 
